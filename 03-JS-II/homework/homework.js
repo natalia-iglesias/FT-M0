2208014767775eea33ec4a7d2b315cda5e21efa7
natalia-iglesias <iglesiasn63@gmail.com>
2021-12-09 10:50:20 -0300

    homework.js-03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -161,18 +161,36 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero < 2) return false;
+  if (numero === 2)return true;
+  for(var i = 2 ; i < numero;i++){
+    if(numero % i === 0){
+      return false;
+    }
+  }
+  return true ;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor===true){
+    return "Soy verdadero";
+  }else {
+    return "Soy falso";
+  }
 
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if(numero > 99 && numero < 999){
+    return true;
+  }else {
+    return false;
+  }
   
 }
 
@@ -180,6 +198,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while(i < 8);
+  return a;
 }
 
 
